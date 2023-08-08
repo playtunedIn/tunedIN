aa0018ea5cd56600431d4978f3452db9634ea6de
Matt Zeiger <matthew.zeiger47@gmail.com>
2023-08-08 12:23:26 -0400

    Initial Commit

--- packages/server/src/handlers/room-handlers/join-room/join-room.ts
+++ packages/server/src/handlers/room-handlers/join-room/join-room.ts
@@ -1,26 +1,48 @@
+// Import necessary modules and types
 import { WebSocket } from 'ws';
-
 import { getValue, publishChannel, setValue } from '../../../clients/redis/redis-client';
 import validator from '../../message.validator';
 import { subscribeGameHandler } from '../../game-handlers/subscribe-game/subscribe-game';
 import { JOIN_ROOM_SCHEMA_NAME, JoinRoomReq } from './join-room.validator';
+import { GameState, PlayerState } from 'src/clients/redis/models/game-state';
 
 export const joinRoomHandler = async (ws: WebSocket, data: JoinRoomReq) => {
   if (!isValidJoinRoomReq(data)) {
     return ws.send('Error');
   }
 
-  const gameState = JSON.parse((await getValue(data.roomId)) as string);
-  gameState.players.push('player 2');
+  const gameStateJson = await getValue(data.roomId);
+
+  if (!gameStateJson) {
+    return ws.send('Room not found');
+  }
+  const gameState: GameState = JSON.parse(gameStateJson);
+
+  if (gameState.players.some(player => player.playerId === data.playerId)) {
+    return ws.send('Player with ID ${data.playerId} is already in the room');
+  }
+
+  const newPlayer: PlayerState = {
+    playerId: data.playerId,
+    score: 0,
+    answers: [],
+  };
+
+  gameState.players.push(newPlayer);
+
+  if (gameState.players.length === 0) {
+    gameState.host = newPlayer.playerId;
+  }
 
   const gameStateStr = JSON.stringify(gameState);
+
   await setValue(gameState.roomId, gameStateStr);
   await publishChannel(gameState.roomId, gameStateStr);
-
   await subscribeGameHandler(ws, gameState.roomId);
 };
 
 const isValidJoinRoomReq = (data: JoinRoomReq) => {
   const validate = validator.getSchema<JoinRoomReq>(JOIN_ROOM_SCHEMA_NAME);
+
   return Boolean(validate?.(data));
 };
