dafc6125303732e93e53b8da163c018b8ec99c50
Shayne Preston <shayne.preston@protonmail.com>
2023-08-16 19:10:01 -0400

    SPOT-40 setup docker compose

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -3,7 +3,9 @@ import dotenv from 'dotenv';
 dotenv.config();
 import cookieParser from 'cookie-parser';
 import express from 'express';
-import { WebSocket, WebSocketServer } from 'ws';
+import type { WebSocket } from 'ws';
+import { WebSocketServer } from 'ws';
+import http from 'http';
 import https from 'https';
 import { readFileSync } from 'fs';
 
@@ -12,9 +14,6 @@ import { messageHandler } from './handlers/message-handler';
 import { validatorInit } from './handlers/message.validator';
 import { unsubscribeChannel } from './clients/redis/redis-client';
 
-const key = readFileSync('./.cert/server.key');
-const cert = readFileSync('./.cert/server.crt');
-
 validatorInit();
 
 const port = process.env.PORT || 3001;
@@ -22,19 +21,36 @@ const port = process.env.PORT || 3001;
 const app = express();
 app.use(cookieParser());
 
-const server = https.createServer({ key, cert }, app);
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+app.get('/test', function (_: any, res: any) {
+  console.log('test');
+  res.send({ test: 'good' });
+});
+
+let server: https.Server | http.Server;
+if (process.env.NODE_ENV === 'development') {
+  const key = readFileSync('./.cert/server.key');
+  const cert = readFileSync('./.cert/server.crt');
+
+  server = https.createServer({ key, cert }, app);
+} else {
+  // TODO: update comment with correct wording if need be for api gateway
+  /**
+   * In production https credentials and keys will be handled by our api gateway allowing backend services
+   * to not have to worry about expiring certificates
+   */
+  server = http.createServer(app);
+}
+
+server.keepAliveTimeout = 90000;
+server.headersTimeout = 95000;
+
 server.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
 
 server.on('error', err => {
   console.error(err);
-});
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-app.get('/test', function (_: any, res: any) {
-  console.log('test');
-  res.send({ test: 'good' });
 });
 
 setupOauthRoutes(app);
