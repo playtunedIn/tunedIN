ce377a0c0de903d2d8187721257af50f6d475eb4
haleykalmbach <124649641+haleykalmbach@users.noreply.github.com>
2023-10-14 18:44:59 -0600

    more trys

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,42 +1,31 @@
-import { useState } from 'react';
 import { registerRootComponent } from 'expo';
 import { MultiplayerProvider } from '@hooks/multiplayer';
-import TempCreateRoomButton from '@components/TempCreateRoomButton';
-import reactLogo from '@assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 import SocketStatus from '@components/common/SocketStatus';
+import './App.css';
 
 export default function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <MultiplayerProvider>
       <>
         <SocketStatus />
-        <TempCreateRoomButton />
-        <div data-testid="app-root">
-          <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-            <img src={viteLogo} className="logo" alt="Vite logo" />
-          </a>
-          <a href="https://react.dev" target="_blank" rel="noreferrer">
-            <img src={reactLogo} className="logo react" alt="React logo" />
-          </a>
-        </div>
-        <h1>Vite + React</h1>
-        <div className="card">
-          <a href="/login">Login with Spotify</a>
-        </div>
-        <div className="card">
-          <button onClick={() => setCount(count => count + 1)}>count is {count}</button>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test HMR
-          </p>
-        </div>
-        <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+        <View style={styles.container}>
+          <Text>Open up App.js to start working on your app!</Text>
+          <StatusBar style="auto" />
+        </View>
       </>
     </MultiplayerProvider>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
 
 registerRootComponent(App);
