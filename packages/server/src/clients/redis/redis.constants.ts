3e27c22fbde87923a2594d1127bd38ce28cedffe
Trevor Boltcreed <tcreeds@gmail.com>
2023-11-19 21:32:06 -0500

    Update create room handler (#57)
    * Update create room handler to include host details
    
    * Update test
    
    * Remove commented out code
    
    * Switch usage of local.playtunedin-test.com to localhost

--- packages/server/src/clients/redis/redis.constants.ts
+++ packages/server/src/clients/redis/redis.constants.ts
@@ -1,4 +1,4 @@
-export const REDIS_HOSTNAME = process.env.REDIS_HOSTNAME || 'redis://local.playtunedin-test.com:6379';
+export const REDIS_HOSTNAME = process.env.REDIS_HOSTNAME || 'redis://localhost:6379';
 export const REDIS_DB_MAP = {
   gameState: 0,
   playerState: 1,
